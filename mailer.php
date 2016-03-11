02a5c74f35b63b5bca6ab422a33f97ba631762f7
NJonsey <nyejones@hotmail.co.uk>
2016-03-11 14:57:50 +0000

    product image added to background

--- mailer.php
+++ mailer.php
@@ -28,26 +28,5 @@
         echo "There was a problem with your submission, please try again.";
     }
 
-        if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
-
-        $recipient = "nyejones01@gmail.com";
-
-        $subject = "Mailing list";
-
-        $email_content .= "Email: $email\n\n";
-        $email_headers = "From: Website-Enquiry";
-
-        if (mail($recipient, $subject, $email_content, $email_headers)) {
-            http_response_code(200);
-            echo "Thank You!\n\nYour email address has been sent.";
-        } else {
-            http_response_code(500);
-            echo "Oops! Something went wrong and we couldn't send your message.";
-        }
-    } else {
-        http_response_code(403);
-        echo "There was a problem with your submission, please try again.";
-    }
 
 ?>

af0fa18c8dce3f65afd5781dccdf3f4c523b7364
NJonsey <nyejones@hotmail.co.uk>
2016-03-11 14:35:55 +0000

    mailing list form added not tested live

--- js/main.js
+++ js/main.js
@@ -15,7 +15,7 @@ $("#contact-form").submit(function(e) {
     error: function(data){
       $button.html('Send');
       $('.form-messages').html(
-        '<i class="fa fa-exclamation-circle fa-2x"></i>The message failed to send. Please try again');
+        '<i class="fa fa-exclamation-circle fa-2x"></i> The message failed to send. Please try again');
       $('.form-messages').fadeIn();
     },
   });
@@ -24,4 +24,31 @@ $("#contact-form").submit(function(e) {
 
 $("#contact-form").delegate(':input', 'focus', function() {
     $('#contact-form-submit').html('Send');
+})
+
+$("#mailing-list").submit(function(e) {
+  $('.mailing-list-messages').fadeOut();
+  $button = $('#mailing-list-submit');
+  $form =  $("#mailing-list");
+  $button.html('<i class="fa fa-spinner fa-spin"></i>');
+  $.ajax({
+    type: "POST",
+    url: "./mailing-list.php",
+    data: $form.serialize(),
+    success: function(data){
+        $form.trigger('reset');
+        $button.html('Sent <i class="fa fa-check"></i>');
+    },
+    error: function(data){
+      $button.html('Send');
+      $('.mailing-list-messages').html(
+        '<i class="fa fa-exclamation-circle fa-2x"></i> Your email failed to send. Please try again');
+      $('.mailing-list-messages').fadeIn();
+    },
+  });
+  e.preventDefault(); 
+});
+
+$("#mailing-list").delegate(':input', 'focus', function() {
+    $('#mailing-list-submit').html('Send');
 })
---
+++ mailing_list.php
@@ -0,0 +1,23 @@
+<?php  
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+
+        $recipient = "nyejones01@gmail.com";
+
+        $subject = "Mailing list";
+
+        $email_content .= "Email: $email\n\n";
+        $email_headers = "From: Mailing-List-Enquiry";
+
+        if (mail($recipient, $subject, $email_content, $email_headers)) {
+            http_response_code(200);
+            echo "Thank You!\n\nYou have joined our mailing list.";
+        } else {
+            http_response_code(500);
+            echo "Oops! Something went wrong and we couldn't send your message.";
+        }
+    } else {
+        http_response_code(403);
+        echo "There was a problem with your submission, please try again.";
+    }
+?>

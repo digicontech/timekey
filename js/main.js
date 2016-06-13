118801aeab2dab30048683dbdb4e1711294f5f7c
NJonsey <nyejones@hotmail.co.uk>
2016-06-13 13:28:56 +0100

    nav transparency increase on scroll

--- js/main.js
+++ js/main.js
@@ -52,3 +52,22 @@ $("#mailing-list").submit(function(e) {
 $("#mailing-list").delegate(':input', 'focus', function() {
     $('#mailing-list-submit').html('Send');
 })
+
+function checkScroll(){
+    var startY = $('.navbar').height() * 8; //The point where the navbar changes in px
+
+    if($(window).scrollTop() > startY){
+        $('.navbar').addClass("scrolled");
+    }else{
+        $('.navbar').removeClass("scrolled");
+    }
+}
+
+if($('.navbar').length > 0){
+    $(window).on("scroll load resize", function(){
+        checkScroll();
+    });
+}
+
+
+
